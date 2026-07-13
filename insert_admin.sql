8cfe94a7a7a03daa687cc70f245767569d34d56c
Nwode Felix <nwodefelix2014@gmail.com>
2026-07-13 12:13:21 +0100

    test gitops autosync - scale frontend replicas

---
+++ insert_admin.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, password_hash, created_at)
+VALUES ('admin', 'pbkdf2:sha256:1000000$etsrmVp3pzCLZ552$ca10eba0975596bbd759306759750234e4d29b6a82e9e3010f422b2919e8543e', NOW());
